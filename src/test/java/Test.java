c416bdc803f5d5c1019add9fd4bbf1351ff30e6e
Vikas Dewangan <dewanganvikas98@gmail.com>
2024-12-28 21:32:07 +0530

    some more test cases added as per description

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -14,4 +14,25 @@ class Test {
     void testSingleNumber() {
         assertEquals(1, calculator.add("1"));
     }
+
+    @Test
+    void testTwoNumbers() {
+        assertEquals(3, calculator.add("1,2"));
+    }
+
+    @Test
+    void testNewLineDelimiter() {
+        assertEquals(6, calculator.add("1\n2,3"));
+    }
+
+    @Test
+    void testCustomDelimiter() {
+        assertEquals(3, calculator.add("//;\n1;2"));
+    }
+
+    @Test
+    void testNegativeNumbersException() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> calculator.add("1,-2,-3"));
+        assertEquals("Negative numbers not allowed: -2, -3", exception.getMessage());
+    }
 }
